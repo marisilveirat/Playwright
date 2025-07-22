180110116de891f70167faca8fa22f3d92c64fca
Mariana Silveira <marilosilveirat@gmail.com>
2025-07-22 16:12:38 -0300

    add: workflow GitHub Actions para testes Playwright

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('Web app deve estar online', async ({ page }) => {
     await page.goto('http://127.0.0.1:8080')
     await expect(page).toHaveTitle("Gerencie suas tarefas com Mark L")
-    await page.waitForTimeout(400)
+    await page.waitForTimeout(4000)
 })
